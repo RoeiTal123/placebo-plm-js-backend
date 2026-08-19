d2102c07f85e0f69818ebc3990706ebffcd6e788
Roei <Roeit1977@gmail.com>
2026-08-19 10:57:07 +0300

    should allow the deployed frontend to talk to the backend

--- index.js
+++ index.js
@@ -31,9 +31,8 @@ app.use(cors({
   origin: [
     "http://localhost:3000",
     "http://localhost:3001",
-    "http://127.0.0.1:5500",
-    // "http://localhost:5500",
-    // "https://final-project-part-2-frontend.onrender.com"
+    "https://placebo-plm-js-v1.vercel.app",
+    "https://placebo-plm-js-v1-git-main-studio-3847s-projects.vercel.app"
   ],
   credentials: true
 }));
